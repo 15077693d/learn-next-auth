e86b6adf7bca46330ce72347d4b9ae35dffb7538
15077693d <oscaryiu.lapsang@gmail.com>
2023-09-23 23:07:48 +0800

    feat: protect api

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -1,9 +1,21 @@
+import { verifyJwt } from "@/lib/jwt";
 import prisma from "@/lib/prisma";
 
 export const GET = async (
   request: Request,
   { params }: { params: { id: number } }
 ) => {
+  const accessToken = request.headers.get("Authorization");
+  if (!accessToken || !verifyJwt(accessToken)) {
+    return new Response(
+      JSON.stringify({
+        error: "unauthorized",
+      }),
+      {
+        status: 401,
+      }
+    );
+  }
   const userPosts = await prisma.post.findMany({
     where: { authorId: Number(params.id) },
     include: {
